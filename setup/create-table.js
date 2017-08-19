ff1846c470c61421b4518912731cf75b0c345bca
Julian GM Alimin <dmastag@yahoo.com>
2017-08-20 00:16:45 +0700

    table create tool for rdbms

---
+++ setup/create-table.js
@@ -0,0 +1,8 @@
+var server = require('../server/server');
+var ds = server.dataSources.db;
+var lbTables = ["User", "AccessToken", "ACL", "RoleMapping", "Role", "userPhoto", "profileData", "maps"];
+ds.automigrate(lbTables, function (er) {
+  if (er) throw er;
+  console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
+  ds.disconnect();
+});
